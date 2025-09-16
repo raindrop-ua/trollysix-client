a5795c241c03e981954415c97f77e802a7f44281
Anton Sizov <raindropua@gmail.com>
2025-09-16 08:18:10 +0300

    chore: update default server port and host configuration

--- src/server.ts
+++ src/server.ts
@@ -54,9 +54,10 @@ app.use('/**', (req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
+  const port = process.env['PORT'] || 4500;
+  const host = process.env['HOST'] ?? '0.0.0.0';
+  app.listen(Number(port), host, () => {
+    console.log(`Node Express server listening on http://${host}:${port}`);
   });
 }
 
