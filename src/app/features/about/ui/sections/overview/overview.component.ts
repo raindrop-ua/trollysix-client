ddb914937e3c55414947212c9ee0e444ac9e8bcc
Anton Sizov <raindropua@gmail.com>
2025-10-09 11:51:32 +0300

    chore: remove unused LazyVideoDirective import from overview component to reduce redundancy

--- src/app/features/about/ui/sections/overview/overview.component.ts
+++ src/app/features/about/ui/sections/overview/overview.component.ts
@@ -3,12 +3,11 @@ import {
   Component,
   ViewEncapsulation,
 } from '@angular/core';
-import { LazyVideoDirective } from '../../../../../shared/directives/lazy-video.directive';
 import { RevealOnScrollDirective } from '../../../../../shared/directives/reveal-on-scroll.directive';
 
 @Component({
   selector: 'app-overview',
-  imports: [LazyVideoDirective, RevealOnScrollDirective],
+  imports: [RevealOnScrollDirective],
   templateUrl: './overview.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
