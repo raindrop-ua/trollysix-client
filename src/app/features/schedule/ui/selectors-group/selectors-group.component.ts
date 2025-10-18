8f8a7e2f3131cc93a33328063962d1d59269e603
Anton Sizov <raindropua@gmail.com>
2025-10-18 12:39:16 +0300

    chore: bind preselected values to options-selector components in selectors-group

--- src/app/features/schedule/ui/selectors-group/selectors-group.component.ts
+++ src/app/features/schedule/ui/selectors-group/selectors-group.component.ts
@@ -9,16 +9,25 @@ import { OptionsSelectorComponent } from '../options-selector/options-selector.c
 import { StopsListComponent } from '../stops-list/stops-list.component';
 import { SvgIconComponent } from '../../../../shared/components/svg-icon/svg-icon.component';
 import { SchedulePageActions } from '../../data-access/store/schedule.actions';
+import { selectScheduleViewModel } from '../../data-access/store/schedule.selectors';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-selectors-group',
-  imports: [OptionsSelectorComponent, StopsListComponent, SvgIconComponent],
+  imports: [
+    OptionsSelectorComponent,
+    StopsListComponent,
+    SvgIconComponent,
+    AsyncPipe,
+  ],
   templateUrl: './selectors-group.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
 export class SelectorsGroupComponent {
   private store = inject(Store);
+
+  vm$ = this.store.select(selectScheduleViewModel);
 
   onSelectDayType(dayTypeName: string): void {
     this.store.dispatch(SchedulePageActions.selectDayType({ dayTypeName }));
