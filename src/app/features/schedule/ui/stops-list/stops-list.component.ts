0ffbca9b29f212edce225720979a0b12157dee24
Anton Sizov <raindropua@gmail.com>
2025-11-24 08:56:30 +0200

    chore: remove unused `SvgIconComponent` import from `StopsListComponent`

--- src/app/features/schedule/ui/stops-list/stops-list.component.ts
+++ src/app/features/schedule/ui/stops-list/stops-list.component.ts
@@ -1,13 +1,11 @@
 import {
   Component,
   inject,
-  input,
   ChangeDetectionStrategy,
   ViewEncapsulation,
 } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AsyncPipe, DatePipe } from '@angular/common';
-import { SvgIconComponent } from '../../../../shared/components/svg-icon/svg-icon.component';
 import { selectScheduleViewModel } from '../../data-access/store/schedule.selectors';
 import { StopCardComponent } from '../stop-card/stop-card.component';
 
