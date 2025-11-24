1664c46efeb34ab09c92196e0715524a1a92e416
Anton Sizov <raindropua@gmail.com>
2025-11-24 08:55:54 +0200

    chore: refactor `StopsListComponent` to use `StopCardComponent`, removing redundant logic and improving clarity

--- src/app/features/schedule/ui/stops-list/stops-list.component.ts
+++ src/app/features/schedule/ui/stops-list/stops-list.component.ts
@@ -9,33 +9,17 @@ import { Store } from '@ngrx/store';
 import { AsyncPipe, DatePipe } from '@angular/common';
 import { SvgIconComponent } from '../../../../shared/components/svg-icon/svg-icon.component';
 import { selectScheduleViewModel } from '../../data-access/store/schedule.selectors';
-import { SchedulePageActions } from '../../data-access/store/schedule.actions';
-import { Stop } from '../../data-access/models/stop.model';
-import { DirectionName } from '../../data-access/models/direction.model';
+import { StopCardComponent } from '../stop-card/stop-card.component';
 
 @Component({
   selector: 'app-stops-list',
-  imports: [SvgIconComponent, AsyncPipe, DatePipe],
+  imports: [AsyncPipe, DatePipe, StopCardComponent],
   templateUrl: './stops-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
 export class StopsListComponent {
-  public readonly showDescriptions = input<boolean>(true);
-  public readonly showDepartures = input<boolean>(true);
   private store = inject(Store);
 
   vm$ = this.store.select(selectScheduleViewModel);
-
-  getDeparture(stop: Stop, direction: DirectionName | null) {
-    if (!direction) return null;
-    return stop.departures?.[direction];
-  }
-
-  onSelectStop(stopId: string, isSelected: boolean): void {
-    if (isSelected) {
-      return;
-    }
-    this.store.dispatch(SchedulePageActions.selectStop({ stopId }));
-  }
 }
