23cca92a28ebc355c56121fc62693f5f45c73189
Anton Sizov <raindropua@gmail.com>
2025-11-24 10:03:38 +0200

    chore: reorganize exports for `testimonials` and align with updated module structure

--- src/app/shared/components/sections/index.ts
+++ src/app/shared/components/sections/index.ts
@@ -1,9 +1,4 @@
-export { MetricsComponent } from './metrics/metrics.component';
 export type { Metric } from './metrics/metrics.model';
-export { TestimonialsComponent } from './testimonials/testimonials.component';
-export type {
-  Testimonials,
-  Testimonial,
-} from './testimonials/testimonial.model';
+export { MetricsComponent } from './metrics/metrics.component';
 export { AttentionComponent } from './attention/attention.component';
 export { NoticeComponent } from './notice/notice.component';
