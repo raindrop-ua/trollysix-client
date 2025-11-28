90103022fee95480fca97c693fee8b284c7683a5
Anton Sizov <raindropua@gmail.com>
2025-11-28 21:12:30 +0200

    chore: enhance `departure-legend.component` with async departures, tighten template logic, and fix parameter spacing in other components

--- src/app/features/schedule/ui/table/departure-legend/departure-legend.component.ts
+++ src/app/features/schedule/ui/table/departure-legend/departure-legend.component.ts
@@ -2,13 +2,19 @@ import {
   Component,
   ChangeDetectionStrategy,
   ViewEncapsulation,
+  inject,
 } from '@angular/core';
+import { ScheduleService } from '../../../services/schedule.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-departure-legend',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './departure-legend.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class DepartureLegendComponent {}
+export class DepartureLegendComponent {
+  private readonly schedule = inject(ScheduleService);
+  readonly departures$ = this.schedule.departures$;
+}
