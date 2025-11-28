dcdc0ab8e7c7abfca2e6c0f91d543145721d11a4
Anton Sizov <raindropua@gmail.com>
2025-11-28 16:51:41 +0200

    chore: add trailing comma to `TemperaturePipe` decorator definition

--- src/app/shared/pipes/temperature.pipe.ts
+++ src/app/shared/pipes/temperature.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'temperature'
+  name: 'temperature',
 })
 export class TemperaturePipe implements PipeTransform {
   transform(value: number | string | undefined | null): string {
