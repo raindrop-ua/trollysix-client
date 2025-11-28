4fc5ce59b25da33328fce383da31ec3cc1b5fa32
Anton Sizov <raindropua@gmail.com>
2025-11-28 16:51:29 +0200

    chore: simplify template syntax in `stop-card.component.html` and update imports and paths for `stop-details.component`

--- src/app/features/schedule/ui/index.ts
+++ src/app/features/schedule/ui/index.ts
@@ -2,4 +2,4 @@ export { OptionsSelectorComponent } from './options-selector/options-selector.co
 export { ScheduleControlsComponent } from './schedule-controls/schedule-controls.component';
 export { SelectorsGroupComponent } from './selectors-group/selectors-group.component';
 export { StopsListComponent } from './stops-list/stops-list.component';
-export { StopDetailsComponent } from './stops-list/stop-details/stop-details.component';
+export { StopDetailsComponent } from './stop-details/stop-details.component';
