681f2b7ecdccb4973eb4313e83aa1488274caa00
Anton Sizov <raindropua@gmail.com>
2025-11-28 19:12:05 +0200

    chore: enhance `stop-details.component` with Google Maps link, computed weather icon URL, and simplified template bindings

--- src/app/features/schedule/ui/stop-details/stop-details.component.ts
+++ src/app/features/schedule/ui/stop-details/stop-details.component.ts
@@ -1,10 +1,12 @@
 import {
   Component,
   input,
+  computed,
   ChangeDetectionStrategy,
   ViewEncapsulation,
 } from '@angular/core';
 import { DecimalPipe, NgOptimizedImage, TitleCasePipe } from '@angular/common';
+import { environment } from '../../../../../environments/environment';
 import { SvgIconComponent } from '../../../../shared/components/svg-icon/svg-icon.component';
 import { TemperaturePipe } from '../../../../shared/pipes/temperature.pipe';
 import { Stop } from '../../data-access/models/stop.model';
@@ -24,4 +26,21 @@ import { Stop } from '../../data-access/models/stop.model';
 })
 export class StopDetailsComponent {
   stopData = input.required<Stop>();
+
+  readonly weatherIconUrl = computed(() => {
+    const weather = this.stopData().weather;
+    if (!weather?.icon) {
+      return `${environment.WEATHER_ICON_BASE}/default@2x.png`;
+    }
+    return `${environment.WEATHER_ICON_BASE}/${weather.icon}@2x.png`;
+  });
+
+  readonly mapsUrl = computed(() => {
+    const geo = this.stopData().geo;
+    if (!geo) {
+      return null;
+    }
+    const { lat, lon } = geo;
+    return `https://www.google.com/maps/search/?api=1&query=${lat},${lon}`;
+  });
 }
