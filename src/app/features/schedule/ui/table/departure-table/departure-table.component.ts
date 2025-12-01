705a154c6d874dd21e6ad085288b0c3120699b27
Anton Sizov <raindropua@gmail.com>
2025-12-01 19:56:53 +0200

    chore: integrate `SpinnerComponent` into `departure-table` for loading state visuals and reorder imports for consistency

--- src/app/features/schedule/ui/table/departure-table/departure-table.component.ts
+++ src/app/features/schedule/ui/table/departure-table/departure-table.component.ts
@@ -6,14 +6,15 @@ import {
 } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { Store } from '@ngrx/store';
-import { DepartureTimeItemComponent } from '../departure-time-item/departure-time-item.component';
-import { ScheduleService } from '../../../services/schedule.service';
 import { selectScheduleViewModel } from '../../../data-access/store/schedule.selectors';
+import { ScheduleService } from '../../../services/schedule.service';
+import { DepartureTimeItemComponent } from '../departure-time-item/departure-time-item.component';
+import { SpinnerComponent } from '../spinner/spinner.component';
 
 @Component({
   selector: 'app-departure-table',
   templateUrl: './departure-table.component.html',
-  imports: [DepartureTimeItemComponent, AsyncPipe],
+  imports: [DepartureTimeItemComponent, AsyncPipe, SpinnerComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
