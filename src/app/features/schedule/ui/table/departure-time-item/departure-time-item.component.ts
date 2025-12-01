6526ee7c0a29eb2616a96045fdaff2f44603501a
Anton Sizov <raindropua@gmail.com>
2025-12-01 14:03:41 +0200

    chore: replace `showSchedule` signal with `toSignal` and integrate `ScheduleService` in `departure-time-item.component` for reactive schedule control

--- src/app/features/schedule/ui/table/departure-time-item/departure-time-item.component.ts
+++ src/app/features/schedule/ui/table/departure-time-item/departure-time-item.component.ts
@@ -2,11 +2,13 @@ import {
   Component,
   input,
   computed,
-  signal,
+  inject,
   ChangeDetectionStrategy,
   ViewEncapsulation,
 } from '@angular/core';
 import { Departure } from '../../../data-access/models/departure.model';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ScheduleService } from '../../../services/schedule.service';
 
 @Component({
   selector: 'app-departure-time-item',
@@ -16,8 +18,13 @@ import { Departure } from '../../../data-access/models/departure.model';
   encapsulation: ViewEncapsulation.None,
 })
 export class DepartureTimeItemComponent {
-  departure = input.required<Departure>();
-  showSchedule = signal(false);
+  private readonly schedule = inject(ScheduleService);
+  public departure = input.required<Departure>();
+
+  readonly showSchedule = toSignal(
+    this.schedule.showScheduleNumbers$,
+    { initialValue: false },
+  );
 
   readonly timeClass = computed(() => {
     const d = this.departure();
