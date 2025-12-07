b89b055f4d9c9e15f309f404a15eb301392b63d9
Anton Sizov <raindropua@gmail.com>
2025-12-07 17:17:27 +0200

    feat: integrate TestimonialsListService into TestimonialsComponent, add dynamic data fetching and fallback spinner

--- src/app/features/home/ui/sections/testimonials/testimonials.component.ts
+++ src/app/features/home/ui/sections/testimonials/testimonials.component.ts
@@ -1,20 +1,33 @@
 import {
   Component,
-  input,
+  inject,
+  signal,
+  OnInit,
   ChangeDetectionStrategy,
-  InputSignal,
+  ViewEncapsulation,
 } from '@angular/core';
-import { Testimonials } from '../../../data-access/models/testimonial.model';
+import { Testimonial } from '../../../data-access/models/testimonial.model';
 import { RevealOnScrollDirective } from '../../../../../shared/directives/reveal-on-scroll.directive';
+import { TestimonialsListService } from '../../../services/testimonials-list.service';
 import { TestimonialComponent } from './testimonial/testimonial.component';
+import { SpinnerComponent } from '../../../../../shared/components/sections';
 
 @Component({
   selector: 'app-testimonials',
-  imports: [RevealOnScrollDirective, TestimonialComponent],
+  imports: [RevealOnScrollDirective, TestimonialComponent, SpinnerComponent],
   templateUrl: './testimonials.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
-export class TestimonialsComponent {
-  public readonly data: InputSignal<Testimonials> =
-    input.required<Testimonials>();
+export class TestimonialsComponent implements OnInit {
+  private testimonialsListService: TestimonialsListService = inject(
+    TestimonialsListService,
+  );
+  public testimonialList = signal<Testimonial[]>([]);
+
+  ngOnInit() {
+    this.testimonialsListService.getTestimonials().subscribe((testimonials) => {
+      this.testimonialList.set(testimonials);
+    });
+  }
 }
