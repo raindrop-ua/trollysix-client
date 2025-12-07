0b180a4ebc529bc502054d9979f13db467b99860
Anton Sizov <raindropua@gmail.com>
2025-12-07 17:15:51 +0200

    refactor: relocate SpinnerComponent to shared folder for better reusability and consistency

--- src/app/shared/components/sections/index.ts
+++ src/app/shared/components/sections/index.ts
@@ -3,3 +3,4 @@ export { GenericSectionBlockComponent } from './generic-section-block/generic-se
 export { NoticeComponent } from './notice/notice.component';
 export type { Metric } from './metrics/metrics.model';
 export { MetricsComponent } from './metrics/metrics.component';
+export { SpinnerComponent } from './spinner/spinner.component';
