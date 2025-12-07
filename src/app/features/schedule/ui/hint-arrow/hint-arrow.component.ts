15352de678cea67846061ee2495fcfddecffd277
Anton Sizov <raindropua@gmail.com>
2025-12-07 17:34:38 +0200

    feat: introduce HintArrowComponent with bounce animation

---
+++ src/app/features/schedule/ui/hint-arrow/hint-arrow.component.ts
@@ -0,0 +1,14 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-hint-arrow',
+  imports: [],
+  templateUrl: './hint-arrow.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class HintArrowComponent {}
