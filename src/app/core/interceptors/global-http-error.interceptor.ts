cf678517009e195f51a447589d04c26949c17c05
Anton Sizov <raindropua@gmail.com>
2025-12-07 20:24:43 +0200

    refactor: enhance global HTTP error handling with dynamic retry delay and improve readability of duration values in interceptor

--- src/app/core/interceptors/global-http-error.interceptor.ts
+++ src/app/core/interceptors/global-http-error.interceptor.ts
@@ -1,24 +1,27 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
-import { retry, tap } from 'rxjs';
+import { retry, tap, timer } from 'rxjs';
 import { inject } from '@angular/core';
 import { ToastService } from '../services/toast.service';
 
 export const globalHttpErrorInterceptor: HttpInterceptorFn = (req, next) => {
-  const toast = inject(ToastService);
+  const toastService: ToastService = inject(ToastService);
 
   return next(req).pipe(
-    retry({ count: 3, delay: 1000 }),
+    retry({
+      count: 3,
+      delay: (_, retryCount) => timer(retryCount * 1_500),
+    }),
     tap({
       error: (error: HttpErrorResponse) => {
         if ([500, 404].includes(error.status)) {
-          toast.error(error.message, {
+          toastService.error(error.message, {
             title: 'Error',
-            duration: 5000,
+            duration: 5_000,
           });
         } else {
-          toast.error('Failed to load data', {
+          toastService.error('Failed to load data', {
             title: 'Error',
-            duration: 5000,
+            duration: 5_000,
           });
         }
       },
