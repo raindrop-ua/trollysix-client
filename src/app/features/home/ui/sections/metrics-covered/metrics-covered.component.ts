f56358e07aa39283b79764e2c843a811c1e99007
Anton Sizov <raindropua@gmail.com>
2025-12-08 06:55:05 +0200

    refactor: simplify MetricsCoveredComponent by switching to async pipe and removing manual subscription handling

--- src/app/features/home/ui/sections/metrics-covered/metrics-covered.component.ts
+++ src/app/features/home/ui/sections/metrics-covered/metrics-covered.component.ts
@@ -1,46 +1,21 @@
 import {
   Component,
   inject,
-  signal,
-  DestroyRef,
-  OnInit,
   ChangeDetectionStrategy,
   ViewEncapsulation,
 } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import {
-  Metric,
-  MetricsComponent,
-  SpinnerComponent,
-} from '../../../../../shared/components/sections';
+import { MetricsComponent } from '../../../../../shared/components/sections';
 import { MetricsListService } from '../../../services/metrics-list.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-metrics-covered',
-  imports: [MetricsComponent, SpinnerComponent],
+  imports: [MetricsComponent, AsyncPipe],
   templateUrl: './metrics-covered.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class MetricsCoveredComponent implements OnInit {
+export class MetricsCoveredComponent {
   private metricsListService = inject(MetricsListService);
-  private destroyRef = inject(DestroyRef);
-  public readonly metrics = signal<Metric[]>([]);
-  public readonly isLoading = signal(true);
-
-  ngOnInit() {
-    this.metricsListService
-      .getMetrics()
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe({
-        next: (metrics) => {
-          this.metrics.set(metrics);
-          this.isLoading.set(false);
-        },
-        error: () => {
-          this.metrics.set([]);
-          this.isLoading.set(false);
-        },
-      });
-  }
+  public metricsList$ = this.metricsListService.getMetrics();
 }
