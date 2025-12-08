334291f39253274dd6e945ce6724e8dadb5a9337
Anton Sizov <raindropua@gmail.com>
2025-12-08 10:50:46 +0200

    feat: add StopsListComponent to display a list of stops with dynamic direction indicator

---
+++ src/app/features/about/ui/sections/full-list-of-stops/stops-list/stops-list.component.ts
@@ -0,0 +1,22 @@
+import {
+  Component,
+  input,
+  ChangeDetectionStrategy,
+  ViewEncapsulation,
+} from '@angular/core';
+import { StopsListItemComponent } from '../stops-list-item/stops-list-item.component';
+import { SvgIconComponent } from '../../../../../../shared/components/svg-icon/svg-icon.component';
+import { RouteStop } from '../../../../data-access/models/route-stops.model';
+
+@Component({
+  selector: 'app-stops-list',
+  imports: [StopsListItemComponent, SvgIconComponent],
+  templateUrl: './stops-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class StopsListComponent {
+  public title = input.required<string>();
+  public stops = input.required<RouteStop[]>();
+  public direction = input.required<'forward' | 'backward'>();
+}
