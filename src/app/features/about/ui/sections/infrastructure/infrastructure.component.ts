eaf14a7a0f096c512aa744b72b3d6d3077183db0
Anton Sizov <raindropua@gmail.com>
2025-12-09 16:00:30 +0200

    feat: include `InfrastructureCardComponent` in `InfrastructureComponent`

--- src/app/features/about/ui/sections/infrastructure/infrastructure.component.ts
+++ src/app/features/about/ui/sections/infrastructure/infrastructure.component.ts
@@ -5,10 +5,15 @@ import {
 } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { GenericSectionBlockComponent } from '../../../../../shared/components/sections';
+import { InfrastructureCardComponent } from './infrastructure-card/infrastructure-card.component';
 
 @Component({
   selector: 'app-infrastructure',
-  imports: [NgOptimizedImage, GenericSectionBlockComponent],
+  imports: [
+    NgOptimizedImage,
+    GenericSectionBlockComponent,
+    InfrastructureCardComponent,
+  ],
   templateUrl: './infrastructure.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
