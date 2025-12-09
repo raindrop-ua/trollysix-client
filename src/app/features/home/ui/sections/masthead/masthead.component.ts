6fe720d625e195910af9415f414137ae9eb65b9f
Anton Sizov <raindropua@gmail.com>
2025-12-09 09:31:05 +0200

    feat: integrate SnowComponent into Masthead for animated snowfall effects

--- src/app/features/home/ui/sections/masthead/masthead.component.ts
+++ src/app/features/home/ui/sections/masthead/masthead.component.ts
@@ -9,6 +9,7 @@ import { AppRouteEnum } from '../../../../../core/enums/app-route.enum';
 import { SvgIconComponent } from '../../../../../shared/components/svg-icon/svg-icon.component';
 import { RevealOnScrollDirective } from '../../../../../shared/directives/reveal-on-scroll.directive';
 import { BtnDirective } from '../../../../../shared/directives/btn.directive';
+import { SnowComponent } from '../snow/snow.component';
 
 @Component({
   selector: 'app-masthead',
@@ -18,6 +19,7 @@ import { BtnDirective } from '../../../../../shared/directives/btn.directive';
     NgOptimizedImage,
     RevealOnScrollDirective,
     BtnDirective,
+    SnowComponent,
   ],
   templateUrl: './masthead.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
