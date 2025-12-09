58e0cb6d1b3dc8cfc68e6e9afbbccc0a2d896a38
Anton Sizov <raindropua@gmail.com>
2025-12-09 08:58:19 +0200

    refactor: adjust formatting in ClockService for improved readability and consistency

--- src/app/features/schedule/services/clock.service.ts
+++ src/app/features/schedule/services/clock.service.ts
@@ -1,6 +1,14 @@
 import { Injectable, inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
-import { interval, map, shareReplay, startWith, switchMap, timer, of } from 'rxjs';
+import {
+  interval,
+  map,
+  shareReplay,
+  startWith,
+  switchMap,
+  timer,
+  of,
+} from 'rxjs';
 
 function msToNextMinute(d = new Date()): number {
   return 60_000 - (d.getSeconds() * 1000 + d.getMilliseconds());
@@ -12,13 +20,13 @@ export class ClockService {
 
   private readonly timeUpdateTicks$ = isPlatformBrowser(this.platformId)
     ? timer(msToNextMinute()).pipe(
-      switchMap(() => interval(5_000)),
-      startWith(0)
-    )
+        switchMap(() => interval(5_000)),
+        startWith(0),
+      )
     : of(0);
 
   readonly now$ = this.timeUpdateTicks$.pipe(
     map(() => new Date()),
-    shareReplay({ bufferSize: 1, refCount: false })
+    shareReplay({ bufferSize: 1, refCount: false }),
   );
 }
