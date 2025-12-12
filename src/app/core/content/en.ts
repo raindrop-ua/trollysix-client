329d13f0f96ab3e58def75ce3a320cf013132428
Anton Sizov <raindropua@gmail.com>
2025-12-12 09:50:04 +0200

    feat: add 'about' section with general description to English translations

--- src/app/core/content/en.ts
+++ src/app/core/content/en.ts
@@ -10,6 +10,14 @@ export const COPY = {
     subtitle: 'Timetable for trolleybus route in the city of Dnipro.',
   },
 
+  about: {
+    generalDescription: [
+      'The route connects the charming residential area of Pridniprovsky, locally referred to simply as Pridniprovsk, with the city center.',
+      'On weekdays, 8 vehicles operate on the route, while on weekends, this number is reduced to 4. The route is served by two trolleybus depots, with 4 vehicles from each depot on weekdays and 2 from each depot on weekends.',
+      'Due to the specifics of the route, as a portion of it lacks overhead contact wires, only a limited number of vehicles capable of autonomous operation over long distances can serve this route.',
+    ],
+  },
+
   errors: {
     notFound: {
       title: 'Page not found',
