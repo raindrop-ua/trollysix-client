ad97dfb3e201f28d81debfba64130eb7150dfb2e
Anton Sizov <raindropua@gmail.com>
2025-12-12 06:00:25 +0200

    feat: integrate GenericHeaderComponent into Terms of Use page template

--- src/app/features/static/terms-of-use/feature/terms-of-use.component.ts
+++ src/app/features/static/terms-of-use/feature/terms-of-use.component.ts
@@ -3,10 +3,11 @@ import {
   ChangeDetectionStrategy,
   ViewEncapsulation,
 } from '@angular/core';
+import { GenericHeaderComponent } from '../../../../shared/components/sections/generic-header/generic-header.component';
 
 @Component({
   selector: 'app-terms-of-use',
-  imports: [],
+  imports: [GenericHeaderComponent],
   templateUrl: './terms-of-use.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
