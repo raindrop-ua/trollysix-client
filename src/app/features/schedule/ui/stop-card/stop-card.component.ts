57349cf0f197211ece123550eb838786f031812a
Anton Sizov <raindropua@gmail.com>
2025-12-14 15:50:24 +0200

    chore: fix import order and update exports in components

--- src/app/features/schedule/ui/stop-card/stop-card.component.ts
+++ src/app/features/schedule/ui/stop-card/stop-card.component.ts
@@ -5,11 +5,11 @@ import {
   ChangeDetectionStrategy,
 } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { AsyncPipe } from '@angular/common';
 import { Stop } from '../../data-access/models/stop.model';
 import { DirectionName } from '../../data-access/models/direction.model';
 import { SchedulePageActions } from '../../data-access/store/schedule.actions';
 import { selectScheduleViewModel } from '../../data-access/store/schedule.selectors';
-import { AsyncPipe } from '@angular/common';
 import { SvgIconComponent } from '../../../../shared/components/svg-icon/svg-icon.component';
 
 @Component({
