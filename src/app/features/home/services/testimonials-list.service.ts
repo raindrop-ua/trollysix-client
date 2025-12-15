dcb6df382220a9d5caa9c628b27d469bbb395f55
Anton Sizov <raindropua@gmail.com>
2025-12-15 10:57:05 +0200

    refactor: update testimonials service to streamline error handling and adjust data model structure

--- src/app/features/home/services/testimonials-list.service.ts
+++ src/app/features/home/services/testimonials-list.service.ts
@@ -1,9 +1,8 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of, shareReplay } from 'rxjs';
+import { Observable, shareReplay } from 'rxjs';
 import { environment } from '../../../../environments/environment';
-import { Testimonial } from '../data-access/models/testimonial.model';
-import { catchError } from 'rxjs/operators';
+import { Testimonials } from '../data-access/models/testimonial.model';
 
 @Injectable({
   providedIn: 'root',
@@ -13,18 +12,10 @@ export class TestimonialsListService {
   private http = inject(HttpClient);
 
   private readonly testimonials$ = this.http
-    .get<Testimonial[]>(`${this.BASE_URL}/testimonials`)
+    .get<Testimonials>(`${this.BASE_URL}/testimonials`)
     .pipe(shareReplay({ bufferSize: 1, refCount: true }));
 
-  public getTestimonials(): Observable<Testimonial[]> {
+  public getTestimonials(): Observable<Testimonials> {
     return this.testimonials$;
-  }
-
-  public getTestimonialsSafe(): Observable<Testimonial[]> {
-    return this.getTestimonials().pipe(
-      catchError(() => {
-        return of<Testimonial[]>([]);
-      }),
-    );
   }
 }
