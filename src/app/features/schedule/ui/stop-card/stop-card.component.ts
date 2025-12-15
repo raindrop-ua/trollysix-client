f83dfeea10902ed2551b959671986250872c1c78
Anton Sizov <raindropua@gmail.com>
2025-12-15 13:02:04 +0200

    feat: add background image support to StopCard component

--- src/app/features/schedule/ui/stop-card/stop-card.component.ts
+++ src/app/features/schedule/ui/stop-card/stop-card.component.ts
@@ -5,7 +5,7 @@ import {
   ChangeDetectionStrategy,
 } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgOptimizedImage } from '@angular/common';
 import { Stop } from '../../data-access/models/stop.model';
 import { DirectionName } from '../../data-access/models/direction.model';
 import { SchedulePageActions } from '../../data-access/store/schedule.actions';
@@ -14,7 +14,7 @@ import { SvgIconComponent } from '../../../../shared/components/svg-icon/svg-ico
 
 @Component({
   selector: 'app-stop-card',
-  imports: [SvgIconComponent, AsyncPipe],
+  imports: [SvgIconComponent, AsyncPipe, NgOptimizedImage],
   templateUrl: './stop-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: { class: 'block' },
@@ -23,6 +23,7 @@ export class StopCardComponent {
   private store = inject(Store);
   public readonly showDescriptions = input<boolean>(false);
   public readonly showDepartures = input<boolean>(true);
+  public readonly showBackgroundImage = input<boolean>(false);
   public selected = input<boolean>(false);
   public stopData = input<Stop>();
 
