08b96274e7d20afa8365eb0b93902eccfa277567
Anton Sizov <raindropua@gmail.com>
2025-12-16 08:58:46 +0200

    refactor: move SnowComponent and SnowService to shared directory and update references

--- src/app/features/home/ui/sections/masthead/masthead.component.ts
+++ src/app/features/home/ui/sections/masthead/masthead.component.ts
@@ -7,7 +7,7 @@ import { ClockService } from '../../../../../core/services/clock.service';
 import { RevealOnScrollDirective } from '../../../../../shared/directives/reveal-on-scroll.directive';
 import { BtnDirective } from '../../../../../shared/directives/btn.directive';
 import { SvgIconComponent } from '../../../../../shared/components/svg-icon/svg-icon.component';
-import { SnowComponent } from '../snow/snow.component';
+import { SnowComponent } from '../../../../../shared/components/snow/snow.component';
 import { copy } from '../../../../../core/content/copy.util';
 
 @Component({
