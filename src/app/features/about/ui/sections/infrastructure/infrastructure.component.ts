50068c403f8f4f5ddd96b9b13eb1dbe54998a7c2
Anton Sizov <raindropua@gmail.com>
2025-12-20 18:40:19 +0200

    feat: display vehicle features in infrastructure section with SVG icons

--- src/app/features/about/ui/sections/infrastructure/infrastructure.component.ts
+++ src/app/features/about/ui/sections/infrastructure/infrastructure.component.ts
@@ -1,7 +1,8 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { GenericSectionBlockComponent } from '../../../../../shared/components/sections';
 import { InfrastructureCardComponent } from './infrastructure-card/infrastructure-card.component';
+import { SvgIconComponent } from '../../../../../shared/components';
 
 @Component({
   selector: 'app-infrastructure',
@@ -9,9 +10,12 @@ import { InfrastructureCardComponent } from './infrastructure-card/infrastructur
     NgOptimizedImage,
     GenericSectionBlockComponent,
     InfrastructureCardComponent,
+    SvgIconComponent,
   ],
   templateUrl: './infrastructure.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: { class: 'block' },
 })
-export class InfrastructureComponent {}
+export class InfrastructureComponent {
+  vehicleFeatures = signal(['accessibility', 'speech']);
+}
