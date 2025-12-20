a3c3cd12c06bde51f943f6e031dd7cbafe15198c
Anton Sizov <raindropua@gmail.com>
2025-12-20 16:38:40 +0200

    refactor: consolidate `value-props` section into `this-is-trollysix` component

--- src/app/features/home/ui/sections/this-is-trollysix/this-is-trollysix.component.ts
+++ src/app/features/home/ui/sections/this-is-trollysix/this-is-trollysix.component.ts
@@ -1,10 +1,31 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ValuePropComponent } from './value-prop/value-prop.component';
+import { ValueProp } from '../../../data-access/models/value-prop.model';
+import { RevealOnScrollDirective } from '../../../../../shared/directives/reveal-on-scroll.directive';
 
 @Component({
   selector: 'app-this-is-trollysix',
-  imports: [],
+  imports: [ValuePropComponent, RevealOnScrollDirective],
   templateUrl: './this-is-trollysix.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: { class: 'block' },
 })
-export class ThisIsTrollysixComponent {}
+export class ThisIsTrollysixComponent {
+  protected readonly valueProps: ValueProp[] = [
+    {
+      title: 'Precision, by default',
+      description:
+        'Precise time, neat statuses: missed, next, upcoming. Everything to get there on time - no surprises.',
+    },
+    {
+      title: 'Designed to disappear',
+      description:
+        "An interface that doesn't distract. Focus only on the route - because time is the most precious thing.",
+    },
+    {
+      title: 'Built for today',
+      description:
+        "Mobile first, lightweight, fast. Works like it's the only thing you need.",
+    },
+  ];
+}
