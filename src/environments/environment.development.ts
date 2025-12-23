05026ab0f15a90068c3bc6b1a4a586cffebcb346
Anton Sizov <raindropua@gmail.com>
2025-12-23 08:37:30 +0200

    refactor(seo): prepend `PUBLIC_URL` to OG image paths for dynamic URL resolution

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   BASE_API_URL: 'http://localhost:4450',
+  PUBLIC_URL: 'http://localhost:4200/',
   WEATHER_ICON_BASE: 'assets/images/weather/icons/',
   SEO_ASSETS: 'assets/seo/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   BASE_API_URL: 'https://api-lite.trolly6.com',
+  PUBLIC_URL: 'https://trolly6.com/',
   WEATHER_ICON_BASE: 'assets/images/weather/icons/',
   SEO_ASSETS: 'assets/seo/',
 };
