79e3a0eb301c700ed641142313f4b4c9d9cae0ee
Anton Sizov <raindropua@gmail.com>
2026-01-09 12:42:02 +0200

    chore(about): extract operating data logic to a dedicated service for improved separation of concerns

--- src/app/features/about/ui/sections/route-operation/route-operation.component.ts
+++ src/app/features/about/ui/sections/route-operation/route-operation.component.ts
@@ -1,6 +1,6 @@
-import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 
-import { OperatingDays } from '@features/about/data-access/models/operating-days.model';
+import { OperatingDataService } from '@features/about/data-access/services/operating-data.service';
 import { GenericSectionBlockComponent } from '@shared/ui/sections/generic-section-block/generic-section-block.component';
 
 import { RouteOperationTableComponent } from './operating-table/route-operation-table.component';
@@ -13,28 +13,6 @@ import { RouteOperationTableComponent } from './operating-table/route-operation-
   host: { class: 'block' },
 })
 export class RouteOperationComponent {
-  public readonly operatingData = signal<OperatingDays>({
-    weekday: {
-      title: 'Weekday',
-      vehiclesQuantity: 8,
-      intervals: ['13 - 14', '27 - 29', '13 - 14', '27 - 29'],
-      operationHours: [
-        '7:00 - 10:00',
-        '10:00 - 15:00',
-        '15:00 - 18:00',
-        '18:00 onwards',
-      ],
-    },
-    weekend: {
-      title: 'Weekend',
-      vehiclesQuantity: 4,
-      intervals: ['26 - 28', '26 - 28', '26 - 28', '26 - 74'],
-      operationHours: [
-        '7:00 - 10:00',
-        '10:00 - 15:00',
-        '15:00 - 18:00',
-        '18:00 onwards',
-      ],
-    },
-  });
+  private operatingDataService = inject(OperatingDataService);
+  public readonly operatingData = this.operatingDataService.operating;
 }
