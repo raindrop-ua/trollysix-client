ed92d7f68467de641efb4b9460a328cb05ffb4c1
Anton Sizov <raindropua@gmail.com>
2026-01-12 08:37:59 +0200

    refactor(schedule): extract `Weather` interface into a separate model and update references in `Stop`

---
+++ src/app/features/schedule/data-access/models/weather.model.ts
@@ -0,0 +1,13 @@
+export interface Weather {
+  temperature: number;
+  description: string;
+  icon: string;
+  feelsLike: number;
+  pressure: number;
+  humidity: number;
+  sunrise: number;
+  sunset: number;
+  windSpeed: number;
+  windDeg: number;
+  windGust: number;
+}
